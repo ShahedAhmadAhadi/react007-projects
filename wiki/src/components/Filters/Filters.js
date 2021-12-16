b4b5e8ef3743747dcf2e9a4cb619e1bdefde4d73
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-16 17:46:47 +0430

    pagination completed with filter core and styles

--- wiki/src/components/Filters/Filters.js
+++ wiki/src/components/Filters/Filters.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Gender from './Catagory/Gender'
+import Species from './Catagory/Species'
+import Status from './Catagory/Status'
 
 const Filters = () => {
     return (
-        <div>
-            filter
+        <div className='col-3'>
+            <div className="text-center fs-4 fw-bold mb-4">Filter</div>
+            <div className='text-center text-primary text-decoration-underline mb-4' style={{ cursor: 'pointer' }}>Clear Filters</div>
+            <div className="accordion" id="accordionExample">
+                <Status />
+                <Species />
+                <Gender />
+            </div>
         </div>
     )
 }
--- wiki/src/components/Pagination/Pagination.js
+++ wiki/src/components/Pagination/Pagination.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactPaginate from 'react-paginate'
+import React from 'react';
+import ReactPaginate from 'react-paginate';
 
-const Pagination = ({setPageNumber, pageNumber, info}) => {
+const Pagination = ({ setPageNumber, pageNumber, info }) => {
     return (
-        <ReactPaginate pageCount={info?.pages} className='pagination justify-content-center' nextLabel="Next" previousLabel="Prev" nextClassName='btn btn-primary' previousLinkClassName='btn btn-primary' pageClassName='page-item' pageLinkClassName='page-link' />
-    )
-}
+        <ReactPaginate
+            pageCount={info?.pages}
+            className="pagination justify-content-center gap-4 my-4"
+            onPageChange={(data) => {
+                setPageNumber(data.selected + 1);
+            }}
+            forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+            activeClassName="active"
+            nextLabel="Next"
+            nextLinkClassName="btn btn-primary"
+            previousLabel="Prev"
+            previousLinkClassName="btn btn-primary"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+        />
+    );
+};
 
-export default Pagination
+export default Pagination;
