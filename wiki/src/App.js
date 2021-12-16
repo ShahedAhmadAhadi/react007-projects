ea55b43f675813afa10612619ad158046b19560a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-16 15:23:41 +0430

    Core Cards completed

--- wiki/src/App.js
+++ wiki/src/App.js
@@ -1,10 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
+import { useEffect, useState } from 'react';
+import Cards from './components/Cards/Cards';
+import Filters from './components/Filters/Filters';
 
 function App() {
+  const [pageNumber, setPageNumber] = useState(1)
+  const [fetchedData, setFetchedData] = useState([])
+  let { info, results } = fetchedData
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res => res.json())
+      setFetchedData(data)
+    })()
+  }, [api])
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">Rick &amp; Morty <span className="text-primary">Wiki</span></h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className='row'>
+              <Cards results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ wiki/src/components/Cards/Cards.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Cards.module.scss'
+
+const Cards = ({results}) => {
+    let display;
+    if (results) {
+        display = results.map(x => {
+            let {id, name, image, location, status} = x
+            return (<div key={id} className='col-4 position-relative'>
+                <div>
+                    <img src={image} alt="" className='img-fluid' />
+                    <div className='content'>
+                        <div className='fs-4 fw-bold mb-4'>{name}</div>
+                        <div className='fs-6'>Last Location</div>
+                        <div className='fs-5'>{location.name}</div>
+                    </div>
+                </div>
+                <div className='position-absolute badge bg-danger'>{status}</div>
+                </div>)
+        })
+    }else{
+        display = "No Characters Found :/"
+    }
+    return (
+        <>
+            {display}
+        </>
+    )
+}
+
+export default Cards
