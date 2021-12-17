9d8add98b480635bda7f64cc936fd5fff6faabbb
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-17 19:03:31 +0430

    CardDetails with it's routes added

--- wiki/src/App.js
+++ wiki/src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Episodes from './Pages/Episodes';
 import Location from './Pages/Locations';
+import CardDetails from './components/Cards/CardDetails';
 
 function App() {
   return (
@@ -18,10 +19,13 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
 
         <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<CardDetails />} />
 
         <Route path="/location" element={<Location />} />
+        <Route path="/location/:id" element={<CardDetails />} />
       </Routes>
     </Router>
   )
