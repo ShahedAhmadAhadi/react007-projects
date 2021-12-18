cf5717f512ac4b29319ed7a1c00c74ec3fb90b8a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-19 01:14:48 +0430

    new App.css added with all project styles and index created for importing in one line

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link, Switch, Route} from 'react-router-dom'
 import {Layout, Typography, Space} from 'antd'
-import Navbar from './components/Navbar'
+import { Navbar } from './components'
 
 function App() {
     return (
