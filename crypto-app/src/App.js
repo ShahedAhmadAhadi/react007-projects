0533a40ce5591d558f967a22cdd85a2e7b573df7
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-19 01:47:24 +0430

    react-router-dom new version added for using switch and imports added to index.js

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './App.css'
+import 'antd/dist/antd.css'
 import {Link, Switch, Route} from 'react-router-dom'
 import {Layout, Typography, Space} from 'antd'
-import { Navbar } from './components'
+import { Cryptocurrencies, CryptoDetails, Exchanges, Homepage, Navbar, News } from './components'
 
 function App() {
     return (
@@ -15,26 +16,34 @@ function App() {
                     <div className='routes'>
                         <Switch>
                             <Route exact path="/">
-                                
+                                <Homepage />
                             </Route>
                             <Route exact path="/exchanges">
-                                
+                                <Exchanges />
                             </Route>
                             <Route exact path="/cryptocurrencies">
-                                
+                                <Cryptocurrencies />
                             </Route>
                             <Route exact path="/crypto/:coinId">
-                                
+                                <CryptoDetails />
                             </Route>
                             <Route exact path="/news">
-                                
+                                <News />
                             </Route>
                         </Switch>
                     </div>
                 </Layout>
-            </div>
             <div className='footer'>
-
+                <Typography.Title level={5} style={{color: "white", textAlign: "center"}}>
+                    CryptoVerse <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
             </div>
         </div>
     )
--- crypto-app/src/components/Homepage.jsx
+++ crypto-app/src/components/Homepage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import milify from 'millify'
+import { Typography } from 'antd'
 
 const Homepage = () => {
     return (
-        <div>
-            HOme
-        </div>
+        <>
+            <Typography.Title level={2} className='heading'>
+                Global Crypto Stats
+            </Typography.Title>
+            
+        </>
     )
 }
 
