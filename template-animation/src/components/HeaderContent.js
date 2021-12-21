f6e938ba69f57b51096bdad50725a1ee5e1f74da
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-21 23:20:36 +0430

    new svg added for button with color and new secondary btn component

--- template-animation/src/components/HeaderContent.js
+++ template-animation/src/components/HeaderContent.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import SecondaryButton from './SecondaryButton'
 
 const HeaderContent = () => {
     return (
         <HeaderContentStyled>
             <div className='left-content'>
+                <h1>Smart banking for freelancers</h1>
+                <p className='white'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quas quaerat at soluta incidunt doloribus explicabo assumenda saepe cum dicta consequatur, culpa possimus enim vitae aperiam nesciunt beatae ea vero.
+                </p>
+                <SecondaryButton name={'Register Now'} />
+            </div>
+            <div className='right-content'>
                 
             </div>
-            <div className='right-content'></div>
         </HeaderContentStyled>
     )
 }
---
+++ template-animation/src/components/SecondaryButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import arrow from '../img/arrow.svg'
+
+const SecondaryButton = ({name}) => {
+    return (
+        <SecondaryButtonStyled>
+            {name}
+            <img src={arrow} alt='' />
+        </SecondaryButtonStyled>
+    )
+}
+
+const SecondaryButtonStyled = styled.button`
+    background-color: var(--dark-primary);
+    padding: .7rem 1.8rem;
+    font-family: inherit;
+    font-size: inherit;
+    color: inherit;
+    border-radius: 20px;
+    outline: none;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img {
+        padding-left: .9rem
+    }
+`
+
+export default SecondaryButton
