0a7aa70627d891080657403065fb523037fff8bd
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-23 17:42:00 +0430

    state dispatched successfully and state added to store

--- todo-list/src/components/Input.js
+++ todo-list/src/components/Input.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { saveTodo } from '../features/todoSlice'
 import './Input.css'
 
 const Input = () => {
-    const [input, setInput] = useState('')
-    const addTodo = () => {
 
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodo = () => {
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now(),
+            
+        }))
     }
     return (
         <div className='input'>
