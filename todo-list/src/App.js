4866a064f17bc7e23beb2edbde4bff44ac389eb8
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-23 17:23:19 +0430

    new components for input of it with state for todo added and styles

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React from 'react'
+import Input from './components/Input';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Input />
     </div>
   );
 }
